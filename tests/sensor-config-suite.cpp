ef4ccc86030c1dcab85650d0c25b064b1fc10bf3
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-06-04 11:24:44 +0200

    defined first requirement

--- tests/sensor-config-suite.cpp
+++ tests/sensor-config-suite.cpp
@@ -7,10 +7,10 @@ TEST(sensor_config_suite, basic)
 {
     SensorConfig sensor_config;
 
-    sensor_config.add_sensor("sensor1", std::make_shared<ConstantSensor>(37.5));
-    sensor_config.add_sensor("sensor2", std::make_shared<ConstantSensor>(-273.15));
+    sensor_config.add_sensor("links-oben", std::make_shared<ConstantSensor>(37.5));
+    sensor_config.add_sensor("rechts-unten", std::make_shared<ConstantSensor>(-273.15));
 
-    auto sensor = sensor_config.get_sensor("sensor1");
+    auto sensor = sensor_config.get_sensor("links-oben");
 
     ASSERT_FLOAT_EQ(sensor->get_temperature(), 37.5);
 }
