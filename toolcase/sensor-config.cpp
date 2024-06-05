514889816d7b370bdecfd82e23f7121ad138592e
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-06-05 11:23:02 +0200

    implement new requirements

--- toolcase/sensor-config.cpp
+++ toolcase/sensor-config.cpp
@@ -8,5 +8,14 @@ void SensorConfig::add_sensor(std::string name, const std::shared_ptr<Sensor>& s
 
 std::shared_ptr<Sensor> SensorConfig::get_sensor(std::string name)
 {
-    return m_sensors[name];
+    auto found = m_sensors.find(name);
+    if (found == m_sensors.end())
+        return std::shared_ptr<Sensor>();
+    else
+        return found->second;
+}
+
+void SensorConfig::remove_sensor(std::string name)
+{
+    m_sensors.erase(name);
 }
--- toolcase/sensor-config.h
+++ toolcase/sensor-config.h
@@ -12,6 +12,9 @@ class SensorConfig
 public:
     void add_sensor(std::string name, const std::shared_ptr<Sensor>& sensor);
     std::shared_ptr<Sensor> get_sensor(std::string name);
+    void remove_sensor(std::string name);
+
+    std::size_t size() const { return m_sensors.size(); }
 
 private:
     std::map<std::string, std::shared_ptr<Sensor>> m_sensors;
